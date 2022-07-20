c92cc7831cd7864cee87fa302ee6a23324b01233
Paulo Gonçalves <paulogoncalves@recife.ifpe.edu.br>
2022-07-20 16:17:26 -0300

    Java version with mobile support

--- src/main/webapp/GUI.js
+++ src/main/webapp/GUI.js
@@ -2,14 +2,19 @@ class GUI {
     constructor() {
         this.xhr = new XMLHttpRequest();
         this.ctx = null;
-        this.c = null;
+        this.c = document.getElementById("hang");
+        this.select = document.querySelector("#lang");
+        this.keys = document.querySelectorAll("button");
         this.type = "REST";
     }
     init() {
-        this.c = document.getElementById("hang");
         let button = document.querySelector("input[type='button']");
         button.onclick = this.start.bind(this);
+        this.keys.forEach(k => k.onclick = this.buttonClicked.bind(this));
         this.start();
+    }
+    buttonClicked(evt) {
+        this.check(evt.currentTarget.dataset.value);
     }
     setMessage(id, msg) {
         let f = document.getElementById(id);
@@ -17,7 +22,7 @@ class GUI {
     }
     start() {
         document.onkeyup = this.sendLetter.bind(this);
-        this.setMessage("letters", "");
+        this.keys.forEach(k => k.className = "");
         this.setMessage("message", "");
         this.setMessage("word", "");
         this.ctx = this.c.getContext("2d");
@@ -32,11 +37,10 @@ class GUI {
         this.ctx.stroke();
         this.xhr.onload = () => {
             let obj = JSON.parse(this.xhr.responseText);
-            this.print("word", obj.word);
+            this.printBoxes(obj.word);
         };
-        let select = document.querySelector("#lang");
         let formData = new FormData();
-        formData.append("value", select.value);
+        formData.append("value", this.select.value);
         if (this.type == "Servlet") {
             this.xhr.open("post", "ServletForca");
             this.xhr.send(formData);
@@ -46,80 +50,102 @@ class GUI {
             this.xhr.send(JSON.stringify(Object.fromEntries(formData)));
         }
     }
-    print(id, vector) {
-        let str = "";
-        for (const letter of vector) {
-            str += " " + letter;
+    printBoxes(vector) {
+        let str = "<tr>";
+        for (let letter of vector) {
+            str += letter === "-" ? "<td class='none'>&ndash;</td>" : "<td></td>";
         }
-        this.setMessage(id, str);
+        str += "</tr>"
+        this.setMessage("word", str);
+    }
+    printLetter(vector) {
+        let divs = document.querySelectorAll("#word td");
+        for (let i = 0; i < vector.length; i++) {
+            let letter = vector[i];
+            if (letter !== '_' && letter !== '-' && !divs[i].dataset.animation) {
+                divs[i].dataset.animation = "flip";
+                divs[i].innerHTML = letter;
+            }
+        }
     }
     sendLetter(ev) {
-        if (ev.key >= 'a' && ev.key <= 'z') {
-            this.xhr.onload = () => {
-                let obj = JSON.parse(this.xhr.responseText);
-                this.print("word", obj.word);
-                this.print("letters", obj.wrongChars);
-                switch (obj.winner) {
-                    case "WIN":
-                        this.setMessage("message", "You win!");
-                        break;
-                    case "LOSE":
-                        document.onkeyup = undefined;
-                        this.setMessage("message", "You lose!");
-                    case "WRONG_LETTER":
-                        switch (obj.wrongChars.length) {
-                            case 1:
-                                /* Cabeça */
-                                this.ctx.beginPath();
-                                this.ctx.arc(70, 40, 10, 0, 2 * Math.PI);
-                                this.ctx.stroke();
-                                break;
-                            case 2:
-                                /* Tronco */
-                                this.ctx.moveTo(70, 50);
-                                this.ctx.lineTo(70, 80);
-                                this.ctx.stroke();
-                                break;
-                            case 3:
-                                /* Braço esquerdo */
-                                this.ctx.moveTo(70, 60);
-                                this.ctx.lineTo(50, 60);
-                                this.ctx.stroke();
-                                break;
-                            case 4:
-                                /* Braço direito */
-                                this.ctx.moveTo(70, 60);
-                                this.ctx.lineTo(90, 60);
-                                this.ctx.stroke();
-                                break;
-                            case 5:
-                                /* Perna esquerda */
-                                this.ctx.moveTo(70, 80);
-                                this.ctx.lineTo(50, 100);
-                                this.ctx.stroke();
-                                break;
-                            case 6:
-                                /* Perna direita */
-                                this.ctx.moveTo(70, 80);
-                                this.ctx.lineTo(90, 100);
-                                this.ctx.stroke();
-                                break;
-                        }
-                        break;
-                    case "CORRECT_LETTER":
-                        break;
-                }
-            };
-            let formData = new FormData();
-            formData.append("value", ev.key);
-            if (this.type == "Servlet") {
-                this.xhr.open("put", "ServletForca");
-                this.xhr.send(formData);
-            } else {
-                this.xhr.open("put", "webresources/hangman");
-                this.xhr.setRequestHeader("Content-Type", "application/json");
-                this.xhr.send(JSON.stringify(Object.fromEntries(formData)));
+        if (ev.key < 'a' || ev.key > 'z') {
+            return;
+        }
+        this.check(ev.key);
+    }
+    printHangman(errors) {
+        switch (errors) {
+            case 1:
+                this.ctx.beginPath();
+                this.ctx.arc(70, 40, 10, 0, 2 * Math.PI);
+                this.ctx.stroke();
+                break;
+            case 2:
+                this.ctx.moveTo(70, 50);
+                this.ctx.lineTo(70, 80);
+                this.ctx.stroke();
+                break;
+            case 3:
+                this.ctx.moveTo(70, 60);
+                this.ctx.lineTo(50, 60);
+                this.ctx.stroke();
+                break;
+            case 4:
+                this.ctx.moveTo(70, 60);
+                this.ctx.lineTo(90, 60);
+                this.ctx.stroke();
+                break;
+            case 5:
+                this.ctx.moveTo(70, 80);
+                this.ctx.lineTo(50, 100);
+                this.ctx.stroke();
+                break;
+            case 6:
+                this.ctx.moveTo(70, 80);
+                this.ctx.lineTo(90, 100);
+                this.ctx.stroke();
+                break;
+        }
+    }
+    check(letter) {
+        if (letter < 'a' || letter > 'z') {
+            return;
+        }
+        this.xhr.onload = () => {
+            let obj = JSON.parse(this.xhr.responseText);
+            this.printLetter(obj.word);
+            let button = document.querySelector(`button[data-value='${letter}']`);
+            switch (obj.winner) {
+                case "WIN":
+                    this.setMessage("message", "You win!");
+                    button.className = "present";
+                    break;
+                case "LOSE":
+                    this.setMessage("message", "You lose!");
+                    button.className = "absent";
+                    this.printHangman(obj.wrongChars.length);
+                    break;
+                case "WRONG_LETTER":
+                    this.setMessage("message", "");
+                    button.className = "absent";
+                    this.printHangman(obj.wrongChars.length);
+                    break;
+                case "CORRECT_LETTER":
+                    this.setMessage("message", "");
+                    button.className = "present";
+                    break;
             }
+        };
+        let formData = new FormData();
+        formData.append("value", letter);
+        if (this.type == "Servlet") {
+            this.xhr.open("put", "ServletForca");
+            this.xhr.send(formData);
+        } else {
+            this.xhr.open("put", "webresources/hangman");
+            this.xhr.setRequestHeader("Content-Type", "application/json");
+            this.xhr.send(JSON.stringify(Object.fromEntries(formData)));
         }
     }
 }
